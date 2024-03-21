ffab5e379c825c6c1eb03450b94fdfeb8d4c7555
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-03-20 23:31:30 -0500

    Finish proyect

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :players, path: :player
+    post 'team/process', to: 'team#player_process'
   end
   
   get "up" => "rails/health#show", as: :rails_health_check
